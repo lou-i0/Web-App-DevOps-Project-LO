cd93df639726d1abec381ac7efc8d3944283901c
lou-i0 <lothen123@googlemail.com>
2023-12-30 11:59:49 +0000

    commented out connection details as dont think they are correct.

--- aks-terraform/main.tf
+++ aks-terraform/main.tf
@@ -10,10 +10,10 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = "dc2fb98a-e0fb-4de0-879e-d817b2136d87"
-  client_secret   = "hfp8Q~.SzKkpgEPRMKS5ZIPfe0xsMipjwED3laLh"
-  subscription_id = "5611fabc-0919-4aea-acbe-4aecceb80e4b"
-  tenant_id       = "40dd58ab-b696-4929-8a7e-47811c3b1357"
+  # client_id       = "dc2fb98a-e0fb-4de0-879e-d817b2136d87" #this is my personal student one i think 
+  # client_secret   = "hfp8Q~.SzKkpgEPRMKS5ZIPfe0xsMipjwED3laLh" #this is my personal student one i think 
+  # subscription_id = "5611fabc-0919-4aea-acbe-4aecceb80e4b" #this is my personal student one i think 
+  # tenant_id       = "40dd58ab-b696-4929-8a7e-47811c3b1357" #this is my personal student one i think 
 }
 
 module "networking" {
