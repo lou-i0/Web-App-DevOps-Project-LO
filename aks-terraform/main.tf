0b0be68946bc24079401b5c6dad8afc5fd7d56fb
lou-i0 <lothen123@googlemail.com>
2024-01-07 17:46:46 +0000

    amend details on connection

--- aks-terraform/main.tf
+++ aks-terraform/main.tf
@@ -10,10 +10,10 @@ terraform {
 provider "azurerm" {
   features {}
 
-  # client_id       = "dc2fb98a-e0fb-4de0-879e-d817b2136d87" #this is my personal student one i think 
-  # client_secret   = "hfp8Q~.SzKkpgEPRMKS5ZIPfe0xsMipjwED3laLh" #this is my personal student one i think 
-  # subscription_id = "5611fabc-0919-4aea-acbe-4aecceb80e4b" #this is my personal student one i think 
-  # tenant_id       = "40dd58ab-b696-4929-8a7e-47811c3b1357" #this is my personal student one i think 
+  # client_id       = 
+  # client_secret   =  
+  # subscription_id = 
+  # tenant_id       = 
 }
 
 module "networking" {
@@ -32,8 +32,8 @@ module "aks_cluster" {
   cluster_location           = "UK South"
   dns_prefix                 = "myaks-project"
   kubernetes_version         = "1.27.7"  # Adjust the version as needed
-  service_principal_client_id = "dc2fb98a-e0fb-4de0-879e-d817b2136d87"
-  service_principal_client_secret = "hfp8Q~.SzKkpgEPRMKS5ZIPfe0xsMipjwED3laLh"
+  service_principal_client_id = 
+  service_principal_client_secret = 
 
   # Input variables referencing outputs from the networking module
   resource_group_name         = module.networking.resource_group_name
